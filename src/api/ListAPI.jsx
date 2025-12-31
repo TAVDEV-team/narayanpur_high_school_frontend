2646fffe9c2a431e09b1575a1aabedf6c7297fa5
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-12-31 14:17:57 +0000

    refactr #169: notices from home page updated

---
+++ src/api/ListAPI.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import API from "./api";
+
+export function ListAPI(endpoint) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const [page, setPage] = useState(1);
+  const [next, setNext] = useState(null);
+  const [previous, setPrevious] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+
+    API.get(`${endpoint}?page=${page}`)
+      .then((res) => {
+        setData(res.data.results);
+        setNext(res.data.next);
+        setPrevious(res.data.previous);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [endpoint, page]);
+
+  return {
+    data,
+    loading,
+    error,
+    page,
+    setPage,
+    next,
+    previous,
+  };
+}
