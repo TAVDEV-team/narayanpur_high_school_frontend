a8a5251f63e7a69646d95f1a22ff2eb8dcf25eb8
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2026-01-07 01:03:02 +0000

    refactor #189: upgraded the listing according to api

--- src/pages/PeopleListing/TeacherInfo.jsx
+++ src/pages/PeopleListing/TeacherInfo.jsx
@@ -20,12 +20,12 @@ export default function TeacherInformation() {
         <ListWrapper title="Teachers" data={teachers} loading={loading} error={error}>
           {teachers.map((teacher) => (
             <PersonCard
-              key={teacher.account.id}
+              key={teacher.id}
               account={teacher.account}
               rightExtra={
                 <>
-                  {teacher.account.display_religion && (
-                    <p><b>Religion:</b> {teacher.account.display_religion}</p>
+                  {teacher.religion && (
+                    <p><b>Religion:</b> {teacher.religion}</p>
                   )}
                   {teacher.base_subject_detail && (
                     <p><b>Subject:</b> {teacher.base_subject_detail.name}</p>
